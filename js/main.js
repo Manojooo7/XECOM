0ed52bba9dba2a4139f1ff6e6df69c6b6f349620
Manojoo7 <manojmondl9834@gmail.com>
2023-11-15 12:40:49 +0530

    slider_functions_added

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
-let splide = new Splide( '.splide' );
-splide.mount();
+
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const slider = document.querySelector('.slider');
+
+let sliderIndex = 0;
+sliderUpdate = () => {
+    slider.style.transform = `translate(${sliderIndex * -25}%)`;
+}
+
+left.addEventListener('click', () => {
+    sliderIndex = (sliderIndex > 0) ? sliderIndex - 1 : 0;
+    sliderUpdate()
+});
+
+right.addEventListener('click', () => {
+    sliderIndex = (sliderIndex < 3) ? sliderIndex + 1 : 3;
+    sliderUpdate()
+});
