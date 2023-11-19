29b3b2848bf7632c829d8e71418e3f8ef4d50698
Manojoo7 <manojmondl9834@gmail.com>
2023-11-19 11:07:41 +0530

    working on slider

--- js/main.js
+++ js/main.js
@@ -1,19 +1,33 @@
-
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const slider = document.querySelector('.slider');
 
 let sliderIndex = 0;
-sliderUpdate = () => {
-    slider.style.transform = `translate(${sliderIndex * -25}%)`;
-}
+const slideCount = 4; // Adjust the number of slides as needed
+const slideWidth = 25; // Adjust the slide width as needed
+const transitionDuration = 0.5; // Adjust the transition duration as needed
 
-left.addEventListener('click', () => {
-    sliderIndex = (sliderIndex > 0) ? sliderIndex - 1 : 0;
-    sliderUpdate()
-});
+const updateSlider = () => {
+    const translateValue = `translate(${sliderIndex * -slideWidth}%)`;
+    slider.style.transition = `transform ${transitionDuration}s ease-in-out`;
+    slider.style.transform = translateValue;
+};
 
-right.addEventListener('click', () => {
-    sliderIndex = (sliderIndex < 3) ? sliderIndex + 1 : 3;
-    sliderUpdate()
-});
+const nextSlide = () => {
+    sliderIndex = (sliderIndex < slideCount - 1) ? sliderIndex + 1 : 0;
+    updateSlider();
+};
+
+const prevSlide = () => {
+    sliderIndex = (sliderIndex > 0) ? sliderIndex - 1 : slideCount - 1;
+    updateSlider();
+};
+
+left.addEventListener('click', prevSlide);
+right.addEventListener('click', nextSlide);
+
+// Automatic slider change every 5 seconds
+const intervalId = setInterval(nextSlide, 5000);
+
+// Optionally, you can clear the interval when needed (e.g., when the user interacts with the slider)
+// clearInterval(intervalId);
