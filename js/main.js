416fda31eccfc0f6993d42956c269921ece3ac47
Manoj Mondal <manojmondal9834@gmail.com>
2023-12-02 17:50:45 +0000

    getting featured products from database

--- js/main.js
+++ js/main.js
@@ -62,16 +62,42 @@ right.addEventListener('click', nextSlide);
 
 
 // for testing purposes
-const apiurl = 'https://ejvtndbnggmfzqqxlgqu.supabase.co/rest/v1/product?select=*&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec&AuthorizationBearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec'
+// const apiurl = 'https://ejvtndbnggmfzqqxlgqu.supabase.co/rest/v1/product?select=*&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec&AuthorizationBearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec'
 
-const getProducts = async () => {
-    const response = await fetch(apiurl);
+
+// getting featured products
+const featuredProductsUrl = 'https://ejvtndbnggmfzqqxlgqu.supabase.co/rest/v1/product?featured=eq.true&select=id,name,price,thumbImg&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec&AuthorizationBearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec'
+
+const getFeaturedProducts = async () => {
+    const response = await fetch(featuredProductsUrl);
     const data = await response.json();
-    console.log(data);
     return data;
 }
 
 window.addEventListener('DOMContentLoaded', async () => {
-    const products = await getProducts();
-    // displayProducts(products);
+    const featuredproducts = await getFeaturedProducts();
+    // console.log(...featuredproducts);
+    featuredproducts.forEach((products)=>{
+        // destructuring datas
+        const {id,name,price,thumbImg} = products;
+
+        // creating a div
+        const featuredProduct = document.createElement('div');
+        // assigning a class to the div 'f_product' which we already styled
+        featuredProduct.classList.add('f_product');
+
+        // creating the featured product card HTML elemts name are same as the raw html elmets name before 
+        featuredProduct.innerHTML=`
+        <div class="fproduct-img">
+            <img src=${thumbImg}>
+        </div>
+        <div class="featured_product_info">
+            <h3>${name}</h3>
+            <p>${price}</p>
+            <button>Buy Now</button>
+        </div>
+        `
+        document.querySelector('.featured_container').appendChild(featuredProduct)
+    })
+
 })
