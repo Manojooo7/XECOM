6ab803a4caddc1de94db05714a22e6f9f412d163
Manoj Mondal <manojmondal9834@gmail.com>
2023-12-05 06:34:24 +0000

    redesigningn according to the UI

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
-const slider = document.querySelector('.slider');
-const sliderControls = document.querySelector('.slider-controls');
+// const left = document.querySelector('.left');
+// const right = document.querySelector('.right');
+// const slider = document.querySelector('.slider');
+// const sliderControls = document.querySelector('.slider-controls');
 
-let sliderIndex = 0;
-let slideCount = 4; // Adjust the number of slides as needed
-const slideWidth = 25; // Adjust the slide width as needed
-const transitionDuration = 0.5; // Adjust the transition duration as needed
+// let sliderIndex = 0;
+// let slideCount = 4; // Adjust the number of slides as needed
+// const slideWidth = 25; // Adjust the slide width as needed
+// const transitionDuration = 0.5; // Adjust the transition duration as needed
 
 // // Duplicate slides for infinite loop
 // for (let i = 0; i < slideCount; i++) {
@@ -20,42 +20,42 @@ const transitionDuration = 0.5; // Adjust the transition duration as needed
 
 
 
-const updateSlider = () => {
-    const translateValue = `translate(${sliderIndex * -slideWidth}%)`;
-    slider.style.transition = `transform ${transitionDuration}s ease-in-out`;
-    slider.style.transform = translateValue;
-};
+// const updateSlider = () => {
+//     const translateValue = `translate(${sliderIndex * -slideWidth}%)`;
+//     slider.style.transition = `transform ${transitionDuration}s ease-in-out`;
+//     slider.style.transform = translateValue;
+// };
 
-const nextSlide = () => {
-    sliderIndex = (sliderIndex < slideCount - 1) ? sliderIndex + 1 : 0;
-    updateSlider();
-};
+// const nextSlide = () => {
+//     sliderIndex = (sliderIndex < slideCount - 1) ? sliderIndex + 1 : 0;
+//     updateSlider();
+// };
 
-const prevSlide = () => {
-    sliderIndex = (sliderIndex > 0) ? sliderIndex - 1 : slideCount - 1;
-    updateSlider();
-};
+// const prevSlide = () => {
+//     sliderIndex = (sliderIndex > 0) ? sliderIndex - 1 : slideCount - 1;
+//     updateSlider();
+// };
 
 
-let isSliderRunning = true;
-let intervalId = setInterval(nextSlide, 5000);
+// let isSliderRunning = true;
+// let intervalId = setInterval(nextSlide, 5000);
 
-const toggleSlider = () => {
-    if (isSliderRunning) {
-        clearInterval(intervalId);
-    } else {
-        intervalId = setInterval(nextSlide, 5000);
-    }
-    isSliderRunning = !isSliderRunning;
-};
+// const toggleSlider = () => {
+//     if (isSliderRunning) {
+//         clearInterval(intervalId);
+//     } else {
+//         intervalId = setInterval(nextSlide, 5000);
+//     }
+//     isSliderRunning = !isSliderRunning;
+// };
 
-slider.addEventListener('mouseenter', toggleSlider);
-slider.addEventListener('mouseleave', toggleSlider);
-sliderControls.addEventListener('mouseleave', toggleSlider);
-sliderControls.addEventListener('mouseenter', toggleSlider);
+// slider.addEventListener('mouseenter', toggleSlider);
+// slider.addEventListener('mouseleave', toggleSlider);
+// sliderControls.addEventListener('mouseleave', toggleSlider);
+// sliderControls.addEventListener('mouseenter', toggleSlider);
 
-left.addEventListener('click', prevSlide);
-right.addEventListener('click', nextSlide);
+// left.addEventListener('click', prevSlide);
+// right.addEventListener('click', nextSlide);
 
 // APIURL = https://ejvtndbnggmfzqqxlgqu.supabase.co/rest/v1/product?select=*&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec&AuthorizationBearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec
 
@@ -73,6 +73,9 @@ const getFeaturedProducts = async () => {
     const data = await response.json();
     return data;
 }
+
+
+// genereting featured products
 
 window.addEventListener('DOMContentLoaded', async () => {
     const featuredproducts = await getFeaturedProducts();
@@ -93,7 +96,7 @@ window.addEventListener('DOMContentLoaded', async () => {
         </div>
         <div class="featured_product_info">
             <h3>${name}</h3>
-            <p>${price}</p>
+            <p>₹${price}</p>
             <button>Buy Now</button>
         </div>
         `
