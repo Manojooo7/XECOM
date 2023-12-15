67c0eadb78015eb5c264b4740c9eb4db654df12e
Manojooo7 <manojmondal9834@gmail.com>
2023-12-15 23:58:08 +0530

    work on featured section

--- html/text.md
+++ html/text.md
@@ -76,3 +76,70 @@ https://ejvtndbnggmfzqqxlgqu.supabase.co/storage/v1/object/sign/Product_images/H
                  </div> -->
             </div>  
         </section>
+
+        // const left = document.querySelector('.left');
+// const right = document.querySelector('.right');
+// const slider = document.querySelector('.slider');
+// const sliderControls = document.querySelector('.slider-controls');
+
+// let sliderIndex = 0;
+// let slideCount = 4; // Adjust the number of slides as needed
+// const slideWidth = 25; // Adjust the slide width as needed
+// const transitionDuration = 0.5; // Adjust the transition duration as needed
+
+// // Duplicate slides for infinite loop
+// for (let i = 0; i < slideCount; i++) {
+//     const clone = slider.children[i].cloneNode(true);
+//     slider.appendChild(clone);
+// }
+
+// const totalSlides = slideCount * 2; // Account for duplicated slides
+// Set the width of the slider to accommodate all slides
+// slider.style.width = `${totalSlides * slideWidth}%`;
+
+
+
+// const updateSlider = () => {
+//     const translateValue = `translate(${sliderIndex * -slideWidth}%)`;
+//     slider.style.transition = `transform ${transitionDuration}s ease-in-out`;
+//     slider.style.transform = translateValue;
+// };
+
+// const nextSlide = () => {
+//     sliderIndex = (sliderIndex < slideCount - 1) ? sliderIndex + 1 : 0;
+//     updateSlider();
+// };
+
+// const prevSlide = () => {
+//     sliderIndex = (sliderIndex > 0) ? sliderIndex - 1 : slideCount - 1;
+//     updateSlider();
+// };
+
+
+// let isSliderRunning = true;
+// let intervalId = setInterval(nextSlide, 5000);
+
+// const toggleSlider = () => {
+//     if (isSliderRunning) {
+//         clearInterval(intervalId);
+//     } else {
+//         intervalId = setInterval(nextSlide, 5000);
+//     }
+//     isSliderRunning = !isSliderRunning;
+// };
+
+// slider.addEventListener('mouseenter', toggleSlider);
+// slider.addEventListener('mouseleave', toggleSlider);
+// sliderControls.addEventListener('mouseleave', toggleSlider);
+// sliderControls.addEventListener('mouseenter', toggleSlider);
+
+// left.addEventListener('click', prevSlide);
+// right.addEventListener('click', nextSlide);
+
+// APIURL = https://ejvtndbnggmfzqqxlgqu.supabase.co/rest/v1/product?select=*&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec&AuthorizationBearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec
+
+
+
+// for testing purposes
+// const apiurl = 'https://ejvtndbnggmfzqqxlgqu.supabase.co/rest/v1/product?select=*&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec&AuthorizationBearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec'
+
--- js/main.js
+++ js/main.js
@@ -1,69 +1,3 @@
-// const left = document.querySelector('.left');
-// const right = document.querySelector('.right');
-// const slider = document.querySelector('.slider');
-// const sliderControls = document.querySelector('.slider-controls');
-
-// let sliderIndex = 0;
-// let slideCount = 4; // Adjust the number of slides as needed
-// const slideWidth = 25; // Adjust the slide width as needed
-// const transitionDuration = 0.5; // Adjust the transition duration as needed
-
-// // Duplicate slides for infinite loop
-// for (let i = 0; i < slideCount; i++) {
-//     const clone = slider.children[i].cloneNode(true);
-//     slider.appendChild(clone);
-// }
-
-// const totalSlides = slideCount * 2; // Account for duplicated slides
-// Set the width of the slider to accommodate all slides
-// slider.style.width = `${totalSlides * slideWidth}%`;
-
-
-
-// const updateSlider = () => {
-//     const translateValue = `translate(${sliderIndex * -slideWidth}%)`;
-//     slider.style.transition = `transform ${transitionDuration}s ease-in-out`;
-//     slider.style.transform = translateValue;
-// };
-
-// const nextSlide = () => {
-//     sliderIndex = (sliderIndex < slideCount - 1) ? sliderIndex + 1 : 0;
-//     updateSlider();
-// };
-
-// const prevSlide = () => {
-//     sliderIndex = (sliderIndex > 0) ? sliderIndex - 1 : slideCount - 1;
-//     updateSlider();
-// };
-
-
-// let isSliderRunning = true;
-// let intervalId = setInterval(nextSlide, 5000);
-
-// const toggleSlider = () => {
-//     if (isSliderRunning) {
-//         clearInterval(intervalId);
-//     } else {
-//         intervalId = setInterval(nextSlide, 5000);
-//     }
-//     isSliderRunning = !isSliderRunning;
-// };
-
-// slider.addEventListener('mouseenter', toggleSlider);
-// slider.addEventListener('mouseleave', toggleSlider);
-// sliderControls.addEventListener('mouseleave', toggleSlider);
-// sliderControls.addEventListener('mouseenter', toggleSlider);
-
-// left.addEventListener('click', prevSlide);
-// right.addEventListener('click', nextSlide);
-
-// APIURL = https://ejvtndbnggmfzqqxlgqu.supabase.co/rest/v1/product?select=*&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec&AuthorizationBearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec
-
-
-
-// for testing purposes
-// const apiurl = 'https://ejvtndbnggmfzqqxlgqu.supabase.co/rest/v1/product?select=*&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec&AuthorizationBearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec'
-
 
 // getting featured products
 const featuredProductsUrl = 'https://ejvtndbnggmfzqqxlgqu.supabase.co/rest/v1/product?featured=eq.true&select=id,name,price,thumbImg&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec&AuthorizationBearer%20eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnRuZGJuZ2dtZnpxcXhsZ3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5ODg5ODEsImV4cCI6MjAxNjU2NDk4MX0.8-oUZN-TtFLdjBRRmoQMoNirKtdnPgrLzzS2pew3Sec'
@@ -77,30 +11,50 @@ const getFeaturedProducts = async () => {
 
 // genereting featured products
 
-window.addEventListener('DOMContentLoaded', async () => {
-    const featuredproducts = await getFeaturedProducts();
-    // console.log(...featuredproducts);
-    featuredproducts.forEach((products) => {
-        // destructuring datas
-        const { id, name, price, thumbImg } = products;
+async function loadFeaturedProducts() {
+    try {
+        const featuredProducts = await getFeaturedProducts();
 
-        // creating a div
-        const featuredProduct = document.createElement('div');
-        // assigning a class to the div 'f_product' which we already styled
-        featuredProduct.classList.add('f_product');
+        // Your code to handle or display featuredProducts should go here
+        featuredProducts.forEach((products) => {
+            // destructuring datas
+            const { id, name, price, thumbImg } = products;
 
-        // creating the featured product card HTML elemts name are same as the raw html elmets name before 
-        featuredProduct.innerHTML = `
-        <div class="fproduct-img">
-            <img src=${thumbImg}>
-           <i class="ri-eye-line"></i>
-        </div>
-        <div class="featured_product_info">
-            <h3>${name}</h3>
-            <p>₹${price}</p>
-        </div>
-        `
-        document.querySelector('.featured_container').appendChild(featuredProduct)
-    })
+            // creating a div
+            const featuredProduct = document.createElement('div');
+            // assigning a class to the div 'f_product' which we already styled
+            featuredProduct.classList.add('f_product');
 
-})
+            // creating the featured product card HTML elemts name are same as the raw html elmets name before 
+            featuredProduct.innerHTML = `
+            <div class="fproduct-img">
+                <img src=${thumbImg}>
+               <i class="ri-eye-line"></i>
+            </div>
+            <div class="featured_product_info">
+                <h3>${name}</h3>
+                <p>₹${price}</p>
+            </div>
+            `
+            document.querySelector('.featured_container').appendChild(featuredProduct)
+
+            featuredProduct.addEventListener('click', () => {
+                redirectToProductDetailPage(id); // Log the product id when the product is clicked
+            });
+            // console.log(...featuredproducts);
+        })
+    } catch (error) {
+        console.error('Error loading featured products:', error);
+    }
+}
+
+// redirect to single product page function
+function redirectToProductDetailPage(id) {
+    const productDetailsURL = `product_detail.html?product=${id}`
+    console.log(productDetailsURL);
+    window.location = productDetailsURL
+}
+
+
+
+window.addEventListener('DOMContentLoaded', loadFeaturedProducts)
